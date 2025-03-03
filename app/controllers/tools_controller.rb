af28d4c0460680631d32f66a1d464dc8f3f79c63
Aldjia Boughias <boughias-aldjia@live.fr>
2025-03-03 17:11:46 +0100

    correcting commit

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,8 +1,12 @@
 class ToolsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @tools = Tool.all
+  end
+
+  def show
+    @tool = Tool.find(params[:id])
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# require "faker"
+
+# 25.times do
+#   tool = Tool.new(
+#     name:
+#     price
+#     description
+#     localisation
+#     photo
+#   )
