854ced92dbe47f640d85ebaabe718e88d87b1055
Cecile Demoor <cecile.demoor@gmail.com>
2025-03-04 12:14:18 +0100

    Création des seeds tools et users

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,4 +1,10 @@
 class Tool < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :localisation, presence: true
+  validates :photo, presence: true
 end
