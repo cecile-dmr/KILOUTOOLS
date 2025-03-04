fdc6f3f7eae8b86ef8a457d6a65964485d5c7d2e
Cecile Demoor <cecile.demoor@gmail.com>
2025-03-04 15:53:14 +0100

    controller pages et view dashboard commencés)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  def home
+  def dashboard
+  # bookings that the user booked
+  @user_bookings = Booking.where(user: current_user)
+
+  # bookings for which the user is the owner
+  @owner_bookings = Booking.includes(tool: :user).where(users: {id: current_user})
+  end
+
+  def ads_user
+    @ads_user = Ad.find(current_user.id)
   end
 end
