66ffeea9792c5c63b1de036205c7da453b3dfe05
Aldjia Boughias <boughias-aldjia@live.fr>
2025-03-05 16:56:33 +0100

    comment dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def dashboard
-  # bookings that the user booked
-  @user_bookings = Booking.where(user: current_user)
-
-  # bookings for which the user is the owner
-  @owner_bookings = Booking.includes(tool: :user).where(users: {id: current_user})
+    # bookings that the user booked
+    @user = current_user
+    # bookings for which the user is the owner
+    @owner_bookings = Booking.includes(tool: :user).where(users: {id: current_user})
   end
 
   def ads_user
