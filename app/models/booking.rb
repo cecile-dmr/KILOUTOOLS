26163b71a5b573827b6944f207a473e14320d55b
baptiste maillard <baptmaillard@gmail.com>
2025-03-06 16:41:33 +0100

    flatpickr fix

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,19 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :tool
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date <= start_date
+      errors.add(:end_date, 'La date choisie doit être ultérieure à la date de début')
+    end
+  end
 end
